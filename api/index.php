520677d24c628020d49a5d2a337ff459d2e5cd4c
averlovistotu-ai <averlovistotu@gmail.com>
2026-07-27 22:35:43 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -298,7 +298,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Credentials: true");
-header("Permissions-Policy: encrypted-media=(self) *, autoplay=(self) *, fullscreen=(self) *");
+header("Permissions-Policy: encrypted-media=*, autoplay=*, fullscreen=*");
 header("Feature-Policy: encrypted-media *; autoplay *; fullscreen *");
 header("Referrer-Policy: no-referrer-when-downgrade");
 ?>
